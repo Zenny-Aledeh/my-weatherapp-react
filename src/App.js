b782d99f130d42b6b3ecf941958cc904173371be
Zenny-Aledeh <113387824+Zenny-Aledeh@users.noreply.github.com>
2023-02-28 18:34:50 +0000

    updated source code info

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ function App() {
     <div>
       <h1>Zenny's Weather App</h1>
       <Search />
+      <br />
+      <p>
+        <a href="https://github.com/Zenny-Aledeh/my-weatherapp-react">
+          Open-source code
+        </a>
+        , by Zeinab Abu
+      </p>
     </div>
   );
 }
